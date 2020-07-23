6454a882f7a0f76411ba998f12f4f10b7786e7bf
garen <garen2994@hotmail.com>
2020-07-23 20:56:57 +0800

    :green_heart: chap1-25 updated

--- src/test/java/com/garen/community/SimCommunityApplicationTests.java
+++ src/test/java/com/garen/community/SimCommunityApplicationTests.java
@@ -1,12 +1,10 @@
 package com.garen.community;
-
 import com.garen.community.dao.AlphaDao;
 import com.garen.community.service.AlphaService;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.BeansException;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.ApplicationContextAware;
